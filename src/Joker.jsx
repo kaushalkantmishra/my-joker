e4d789212317b45d7a778ba78ea8d4702bb7564c
kaushalkantmishra <kaushalkantmishra127@gmail.com>
2024-02-09 13:05:32 +0530

    first commit

---
+++ src/Joker.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./Joker.css";
+
+export default function Joker() {
+  let [joke, setJoke] = useState({});
+  const URL = "https://official-joke-api.appspot.com/random_joke";
+
+  const getNewJock = async () => {
+    let response = await fetch(URL);
+    let jsonResponse = await response.json();
+    setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+  };
+  useEffect(() => {
+    async function getFirstJock() {
+      let response = await fetch(URL);
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+      setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    }
+    getFirstJock();
+  }, []);
+  
+  return (
+    <div className="joker-container">
+      <div className="joker-header">
+        <h1> My Joker!</h1>
+      </div>
+      <div className="joker-joke-container">
+        <h3 className="joker-setup">{joke.setup}</h3>
+        <h4 className="joker-punchline">{joke.punchline}</h4>
+      </div>
+      <div>
+        <button className="joker-button" onClick={getNewJock}>New Joke</button>
+      </div>
+    </div>
+  );
+}
